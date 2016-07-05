a14d8e01337765a37162f0dd502b95d5822d2dee
ditihala <dt72@nyu.edu>
2016-07-05 19:57:19 +0400

    First push, the whole application. Instructions on how to use it on ReadeMe

---
+++ SGX-Packer/Debug/timcmd.bat
@@ -0,0 +1,5 @@
+set startTime=%time%
+restuner_packed.exe
+set FinishTime=%time%
+echo Start Time: %startTime%
+echo Finish Time: %FinishTime%
---
+++ SGX-Packer/Enclave1/Enclave1.cpp
@@ -0,0 +1,480 @@
+#include "Enclave1_t.h"
+#include <string.h>
+#include <stdio.h>
+#include "sgx_report.h"
+#include "sgx_tkey_exchange.h"
+#include "sgx_tcrypto.h"
+#include "string.h"
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning ( disable:4127 )
+#endif
+
+#ifndef uint8_t
+#define uint8_t  unsigned char
+#endif
+
+void aes_expandDecKey(uint8_t *k, uint8_t *rc) ;
+void aes_expandEncKey(uint8_t *k, uint8_t *rc) ;
+uint8_t rj_xtime(uint8_t x) ;
+void aes_addRoundKey(uint8_t *buf, uint8_t *key);
+void aes_addRoundKey_cpy(uint8_t *buf, uint8_t *key, uint8_t *cpk);
+void aes_subBytes_inv(uint8_t *buf);
+void aes_shiftRows_inv(uint8_t *buf);
+void aes_mixColumns_inv(uint8_t *buf);
+
+
+
+
+
+#define F(x)   (((x)<<1) ^ ((((x)>>7) & 1) * 0x1b))
+#define FD(x)  (((x) >> 1) ^ (((x) & 1) ? 0x8d : 0))
+
+
+// This is the public EC key of the SP. The corresponding private EC key is
+// used by the SP to sign data used in the remote attestation SIGMA protocol
+// to sign channel binding data in MSG2. A successful verification of the
+// signature confirms the identity of the SP to the ISV app in remote
+// attestation secure channel binding. The public EC key should be hardcoded in
+// the enclave or delivered in a trustworthy manner. The use of a spoofed public
+// EC key in the remote attestation with secure channel binding session may lead
+// to a security compromise. Every different SP the enlcave communicates to
+// must have a unique SP public key. Delivery of the SP public key is
+// determined by the ISV. The TKE SIGMA protocl expects an Elliptical Curve key
+// based on NIST P-256
+static const sgx_ec256_public_t g_sp_pub_key = {
+    {
+        0x72, 0x12, 0x8a, 0x7a, 0x17, 0x52, 0x6e, 0xbf,
+        0x85, 0xd0, 0x3a, 0x62, 0x37, 0x30, 0xae, 0xad,
+        0x3e, 0x3d, 0xaa, 0xee, 0x9c, 0x60, 0x73, 0x1d,
+        0xb0, 0x5b, 0xe8, 0x62, 0x1c, 0x4b, 0xeb, 0x38
+    },
+    {
+        0xd4, 0x81, 0x40, 0xd9, 0x50, 0xe2, 0x57, 0x7b,
+        0x26, 0xee, 0xb7, 0x41, 0xe7, 0xc6, 0x14, 0xe2,
+        0x24, 0xb7, 0xbd, 0xc9, 0x03, 0xf2, 0x9a, 0x28,
+        0xa8, 0x3c, 0xc8, 0x10, 0x11, 0x14, 0x5e, 0x06
+    }
+};
+
+
+const uint8_t sbox[256] = {
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5,
+    0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0,
+    0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc,
+    0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a,
+    0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0,
+    0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b,
+    0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85,
+    0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5,
+    0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17,
+    0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88,
+    0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c,
+    0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9,
+    0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6,
+    0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e,
+    0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94,
+    0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68,
+    0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
+};
+
+const uint8_t sboxinv[256] = {
+    0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38,
+    0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+    0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87,
+    0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+    0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d,
+    0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+    0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2,
+    0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+    0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16,
+    0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+    0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda,
+    0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+    0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a,
+    0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+    0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02,
+    0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+    0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea,
+    0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+    0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85,
+    0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+    0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89,
+    0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+    0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20,
+    0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+    0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31,
+    0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+    0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d,
+    0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+    0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0,
+    0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26,
+    0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+};
+
+#define rj_sbox(x)     sbox[(x)]
+#define rj_sbox_inv(x) sboxinv[(x)]
+
+// Used to store the secret passed by the SP in the sample code. The
+// size is forced to be 8 bytes. Expected value is
+// 0x01,0x02,0x03,0x04,0x0x5,0x0x6,0x0x7
+uint8_t g_secret[8] = {0};
+
+
+void decrypter(char *buffer, unsigned char *buffer2, size_t len, size_t len2){
+	const char *message = "Decrypting inside the enclave...\n";
+	if (len > strlen(message)){
+		memcpy(buffer, message, strlen(message) + 1);
+	}
+
+	uint8_t i;
+	uint8_t rcon = 1;
+	uint8_t deckey[32];
+	uint8_t enkey[32];
+
+	unsigned char k[32] = {
+		g_secret[0], 0x28, 0x40, 0x6e, g_secret[1], 0x64, 0x63, 0x5d, g_secret[2], 0x61, 0x77, 0x40,g_secret[3], 0x71, 0x77, 0x28, 
+		g_secret[4], 0x61, 0x7d, 0x66, g_secret[5], 0x73, 0x3b, 0x5d, g_secret[6], 0x6d, 0x3c, 0x3f, g_secret[7], 0x66, 0x72, 0x36
+	};
+	unsigned char buffor[16];
+	
+	memcpy(buffor, buffer2, 16);
+	
+	for (i = 0; i < sizeof(k); i++) deckey[i] = k[i];
+    for (i = 8;--i;) aes_expandEncKey(deckey, &rcon);
+
+    aes_addRoundKey_cpy(buffer2, deckey, k);
+    aes_shiftRows_inv(buffer2);
+    aes_subBytes_inv(buffer2);
+	
+    for (i = 14, rcon = 0x80; --i;)
+    {
+        if( ( i & 1 ) )           
+        {
+            aes_expandDecKey(k, &rcon);
+            aes_addRoundKey(buffer2, &k[16]);
+        }
+        else aes_addRoundKey(buffer2, k);
+        aes_mixColumns_inv(buffer2);
+        aes_shiftRows_inv(buffer2);
+        aes_subBytes_inv(buffer2);
+    }
+    aes_addRoundKey( buffer2, k); 
+	
+	
+	//memcpy(buffer2, buffor, 16);
+	
+}
+
+void aes_expandDecKey(uint8_t *k, uint8_t *rc) 
+{
+    uint8_t i;
+
+    for(i = 28; i > 16; i -= 4) k[i+0] ^= k[i-4], k[i+1] ^= k[i-3], 
+        k[i+2] ^= k[i-2], k[i+3] ^= k[i-1];
+
+    k[16] ^= rj_sbox(k[12]);
+    k[17] ^= rj_sbox(k[13]);
+    k[18] ^= rj_sbox(k[14]);
+    k[19] ^= rj_sbox(k[15]);
+
+    for(i = 12; i > 0; i -= 4)  k[i+0] ^= k[i-4], k[i+1] ^= k[i-3],
+        k[i+2] ^= k[i-2], k[i+3] ^= k[i-1];
+
+    *rc = FD(*rc);
+    k[0] ^= rj_sbox(k[29]) ^ (*rc);
+    k[1] ^= rj_sbox(k[30]);
+    k[2] ^= rj_sbox(k[31]);
+    k[3] ^= rj_sbox(k[28]);
+} /* aes_expandDecKey */
+
+void aes_addRoundKey(uint8_t *buf, uint8_t *key)
+{
+    register uint8_t i = 16;
+
+    while (i--) buf[i] ^= key[i];
+} /* aes_addRoundKey */
+
+
+void aes_addRoundKey_cpy(uint8_t *buf, uint8_t *key, uint8_t *cpk)
+{
+    register uint8_t i = 16;
+
+    while (i--)  buf[i] ^= (cpk[i] = key[i]), cpk[16+i] = key[16 + i];
+} /* aes_addRoundKey_cpy */
+
+void aes_subBytes_inv(uint8_t *buf)
+{
+    register uint8_t i = 16;
+
+    while (i--) buf[i] = rj_sbox_inv(buf[i]);
+} /* aes_subBytes_inv */
+
+void aes_shiftRows_inv(uint8_t *buf)
+{
+    register uint8_t i, j; /* same as above :) */
+
+    i = buf[1]; buf[1] = buf[13]; buf[13] = buf[9]; buf[9] = buf[5]; buf[5] = i;
+    i = buf[2]; buf[2] = buf[10]; buf[10] = i;
+    j = buf[3]; buf[3] = buf[7]; buf[7] = buf[11]; buf[11] = buf[15]; buf[15] = j;
+    j = buf[6]; buf[6] = buf[14]; buf[14] = j;
+
+} /* aes_shiftRows_inv */
+
+void aes_mixColumns_inv(uint8_t *buf)
+{
+    register uint8_t i, a, b, c, d, e, x, y, z;
+
+    for (i = 0; i < 16; i += 4)
+    {
+        a = buf[i]; b = buf[i + 1]; c = buf[i + 2]; d = buf[i + 3];
+        e = a ^ b ^ c ^ d;
+        z = rj_xtime(e);
+        x = e ^ rj_xtime(rj_xtime(z^a^c));  y = e ^ rj_xtime(rj_xtime(z^b^d));
+        buf[i] ^= x ^ rj_xtime(a^b);   buf[i+1] ^= y ^ rj_xtime(b^c);
+        buf[i+2] ^= x ^ rj_xtime(c^d); buf[i+3] ^= y ^ rj_xtime(d^a);
+    }
+} /* aes_mixColumns_inv */
+
+void aes_expandEncKey(uint8_t *k, uint8_t *rc) 
+{
+    register uint8_t i;
+
+    k[0] ^= rj_sbox(k[29]) ^ (*rc);
+    k[1] ^= rj_sbox(k[30]);
+    k[2] ^= rj_sbox(k[31]);
+    k[3] ^= rj_sbox(k[28]);
+    *rc = F( *rc);
+
+    for(i = 4; i < 16; i += 4)  k[i] ^= k[i-4],   k[i+1] ^= k[i-3],
+        k[i+2] ^= k[i-2], k[i+3] ^= k[i-1];
+    k[16] ^= rj_sbox(k[12]);
+    k[17] ^= rj_sbox(k[13]);
+    k[18] ^= rj_sbox(k[14]);
+    k[19] ^= rj_sbox(k[15]);
+
+    for(i = 20; i < 32; i += 4) k[i] ^= k[i-4],   k[i+1] ^= k[i-3],
+        k[i+2] ^= k[i-2], k[i+3] ^= k[i-1];
+
+} /* aes_expandEncKey */
+
+uint8_t rj_xtime(uint8_t x) 
+{
+    return (x & 0x80) ? ((x << 1) ^ 0x1b) : (x << 1);
+} /* rj_xtime */
+
+// This ecall is a wrapper of sgx_ra_init to create the trusted
+// KE exchange key context needed for the remote attestation
+// SIGMA API's. Input pointers aren't checked since the trusted stubs
+// copy them into EPC memory.
+//
+// @param b_pse Indicates whether the ISV app is using the
+//              platform services.
+// @param p_context Pointer to the location where the returned
+//                  key context is to be copied.
+//
+// @return Any error return from the create PSE session if b_pse
+//         is true.
+// @return Any error returned from the trusted key exchange API
+//         for creating a key context.
+
+sgx_status_t enclave_init_ra(
+    int b_pse,
+    sgx_ra_context_t *p_context)
+{
+    // isv enclave call to trusted key exchange library.
+    sgx_status_t ret;
+    if(b_pse)
+    {
+        int busy_retry_times = 2;
+        do{
+            ret = sgx_create_pse_session();
+        }while (ret == SGX_ERROR_BUSY && busy_retry_times--);
+        if (ret != SGX_SUCCESS)
+            return ret;
+    }
+    ret = sgx_ra_init(&g_sp_pub_key, b_pse, p_context);
+    if(b_pse)
+    {
+        sgx_close_pse_session();
+        return ret;
+    }
+    return ret;
+}
+
+
+// Closes the tKE key context used during the SIGMA key
+// exchange.
+//
+// @param context The trusted KE library key context.
+//
+// @return Return value from the key context close API
+
+sgx_status_t SGXAPI enclave_ra_close(
+    sgx_ra_context_t context)
+{
+    sgx_status_t ret;
+    ret = sgx_ra_close(context);
+    return ret;
+}
+
+
+// Verify the mac sent in att_result_msg from the SP using the
+// MK key. Input pointers aren't checked since the trusted stubs
+// copy them into EPC memory.
+//
+//
+// @param context The trusted KE library key context.
+// @param p_message Pointer to the message used to produce MAC
+// @param message_size Size in bytes of the message.
+// @param p_mac Pointer to the MAC to compare to.
+// @param mac_size Size in bytes of the MAC
+//
+// @return SGX_ERROR_INVALID_PARAMETER - MAC size is incorrect.
+// @return Any error produced by tKE  API to get SK key.
+// @return Any error produced by the AESCMAC function.
+// @return SGX_ERROR_MAC_MISMATCH - MAC compare fails.
+
+sgx_status_t verify_att_result_mac(sgx_ra_context_t context,
+                                   uint8_t* p_message,
+                                   size_t message_size,
+                                   uint8_t* p_mac,
+                                   size_t mac_size)
+{
+    sgx_status_t ret;
+    sgx_ec_key_128bit_t mk_key;
+
+    if(mac_size != sizeof(sgx_mac_t))
+    {
+        ret = SGX_ERROR_INVALID_PARAMETER;
+        return ret;
+    }
+    if(message_size > UINT32_MAX)
+    {
+        ret = SGX_ERROR_INVALID_PARAMETER;
+        return ret;
+    }
+
+    do {
+        uint8_t mac[SGX_CMAC_MAC_SIZE] = {0};
+
+        ret = sgx_ra_get_keys(context, SGX_RA_KEY_MK, &mk_key);
+        if(SGX_SUCCESS != ret)
+        {
+            break;
+        }
+        ret = sgx_rijndael128_cmac_msg(&mk_key,
+                                       p_message,
+                                       (uint32_t)message_size,
+                                       &mac);
+        if(SGX_SUCCESS != ret)
+        {
+            break;
+        }
+        if(0 == consttime_memequal(p_mac, mac, sizeof(mac)))
+        {
+            ret = SGX_ERROR_MAC_MISMATCH;
+            break;
+        }
+
+    }
+    while(0);
+
+    return ret;
+}
+
+
+// Generate a secret information for the SP encrypted with SK.
+// Input pointers aren't checked since the trusted stubs copy
+// them into EPC memory.
+//
+// @param context The trusted KE library key context.
+// @param p_secret Message containing the secret.
+// @param secret_size Size in bytes of the secret message.
+// @param p_gcm_mac The pointer the the AESGCM MAC for the
+//                 message.
+//
+// @return SGX_ERROR_INVALID_PARAMETER - secret size if
+//         incorrect.
+// @return Any error produced by tKE  API to get SK key.
+// @return Any error produced by the AESGCM function.
+// @return SGX_ERROR_UNEXPECTED - the secret doesn't match the
+//         expected value.
+
+sgx_status_t put_secret_data(
+    sgx_ra_context_t context,
+    uint8_t *p_secret,
+    uint32_t secret_size,
+    uint8_t *p_gcm_mac)
+{
+    sgx_status_t ret = SGX_SUCCESS;
+    sgx_ec_key_128bit_t sk_key;
+
+    do {
+        if(secret_size != 8)
+        {
+            ret = SGX_ERROR_INVALID_PARAMETER;
+            break;
+        }
+
+        ret = sgx_ra_get_keys(context, SGX_RA_KEY_SK, &sk_key);
+        if(SGX_SUCCESS != ret)
+        {
+            break;
+        }
+
+        uint8_t aes_gcm_iv[12] = {0};
+        ret = sgx_rijndael128GCM_decrypt(&sk_key,
+                                         p_secret,
+                                         secret_size,
+                                         &g_secret[0],
+                                         &aes_gcm_iv[0],
+                                         12,
+                                         NULL,
+                                         0,
+                                         (const sgx_aes_gcm_128bit_tag_t *)
+                                            (p_gcm_mac));
+
+        uint32_t i;
+        bool secret_match = true;
+        for(i=0;i<secret_size;i++)
+        {
+            if(g_secret[i] != i)
+            {
+                secret_match = false;
+            }
+        }
+
+        if(!secret_match)
+        {
+            ret = SGX_ERROR_UNEXPECTED;
+        }
+
+        // Once the server has the shared secret, it should be sealed to
+        // persistent storage for future use. This will prevents having to
+        // perform remote attestation until the secret goes stale. Once the
+        // enclave is created again, the secret can be unsealed.
+    } while(0);
+    return ret;
+}
+
+#ifdef _MSC_VER
+    #pragma warning(pop)
+#endif
